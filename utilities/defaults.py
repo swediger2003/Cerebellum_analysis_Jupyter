0e7856edfbbba3394dfff944a6422f4016ca3ff4
gaynor-c <gaynor.c@northeastern.edu>
2024-05-28 09:42:14 -0400

    default management, remove unnecessary notebook

---
+++ utilities/defaults.py
@@ -0,0 +1,6 @@
+import networkx as nx
+
+default_graph_name = 'test_graph.gml'
+G = nx.read_gml(default_graph_name)
+
+pc_list = ['pc_2', 'pc_9', 'pc_16', 'pc_22', 'pc_23', 'pc_26', 'pc_32', 'pc_34', 'pc_35', 'pc_50']
